3de5220717ebdecdd8c93ce2ffd5969059d73b38
YuraYeghiazaryan <101919335+YuraYeghiazaryan@users.noreply.github.com>
2022-03-20 16:16:24 +0400

    Create Homeword_4.py

---
+++ Homeword_4.py
@@ -0,0 +1,34 @@
+Վարժություն 1.11
+# Recursion variant
+def f(n):
+    if n < 0:
+        return "Wrong Input!"
+    else:
+        if n < 3:
+            return n
+        return f(n - 1) + f(n - 2) + f(n - 3)
+
+
+print(f(7))
+
+
+# Loop variant
+
+def fib_3(n):
+    a = 0
+    b = 1
+    c = 2
+    while n > 0:
+        a, b, c = b, c, a + b + c
+        n -= 1
+    return a
+
+
+print(fib_3(7))
+
+# Tail recourse
+def fib_3p(n, a=0, b=1, c=2):
+    return a if n == 0 else fib_3p(n - 1, b, c, a + b + c)
+
+
+print(fib_3p(7))
